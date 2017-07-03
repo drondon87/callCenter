8f844c64c5e7a329b2793c9a012fb4455831530e
Domingo Rondon <domingorondon@grupodigifarm.com>
2017-07-02 21:26:54 -0400

    Se crean los hilios para dichas clases

--- src/main/java/com/almundo/callcenter/model/Operador.java
+++ src/main/java/com/almundo/callcenter/model/Operador.java
@@ -1,5 +1,10 @@
 package com.almundo.callcenter.model;
 
+import org.apache.log4j.Logger;
+
+import com.almundo.callcenter.generador.CallQueue;
+import com.almundo.callcenter.process.EstatusPersona;
+
 /**
  * Operador.java
  * Clase que contiene atributos particulares del Operador
@@ -7,6 +12,70 @@ package com.almundo.callcenter.model;
  * 1 jul. 2017
  * @author Domingo Rondon
  */
-public class Operador extends Persona{
+public class Operador extends Persona implements Runnable{
+
+	final static Logger logger = Logger.getLogger(Operador.class);
+	
+	private boolean running;
+	private EstatusPersona status;
+	private long callExiration;
+	private int id;
+
+	public Operador(int id){
+		this.id = id;
+		this.status = EstatusPersona.FREE;
+	}
+	
+	public void run() {
+		while (running){
+			
+			if(status == EstatusPersona.FREE){
+				
+				Call call = CallQueue.removeCall();
+				if(call!=null){
+					logger.info("Operador "+id +" Contestando Llamada ... "+call.getNumero());
+					callExiration = System.currentTimeMillis() + (call.getDuracion() * 40 * 100); 
+					status = EstatusPersona.IN_CALL;
+				}
+				
+			}else{
+				
+				if(System.currentTimeMillis() > callExiration){
+					status = EstatusPersona.FREE;
+					logger.info("Operador "+id+" Colgando Llamada ... ");
+				}
+			}
+			
+			sleep();
+		}
+		
+	}
+	
+	private void sleep(){
+		try{
+			Thread.sleep(1000);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stop(){
+		running = false;
+	}
+	
+	public void start(){
+		running = true;
+		Thread trThread = new Thread(this);
+		trThread.setPriority(Thread.MAX_PRIORITY);
+		trThread.start();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
--- src/main/java/com/almundo/callcenter/model/Supervisor.java
+++ src/main/java/com/almundo/callcenter/model/Supervisor.java
@@ -1,5 +1,10 @@
 package com.almundo.callcenter.model;
 
+import org.apache.log4j.Logger;
+
+import com.almundo.callcenter.generador.CallQueue;
+import com.almundo.callcenter.process.EstatusPersona;
+
 /**
  * Supervisor.java
  * Clase que contiene atributos particulares del Supervisor del CallCenter
@@ -7,6 +12,70 @@ package com.almundo.callcenter.model;
  * 1 jul. 2017
  * @author Domingo Rondon
  */
-public class Supervisor extends Persona{
+public class Supervisor extends Persona implements Runnable{
+
+	final static Logger logger = Logger.getLogger(Supervisor.class);
+	
+	private boolean running;
+	private EstatusPersona status;
+	private long callExiration;
+	private int id;
+	
+	public Supervisor(int id){
+		this.id = id;
+		this.status = EstatusPersona.FREE;
+	}
+	
+	public void run() {
+		while (running){
+			
+			if(status == EstatusPersona.FREE){
+				
+				Call call = CallQueue.removeCall();
+				if(call!=null){
+					logger.info("Supervisor "+id +" Contestando Llamada ... "+call.getNumero());
+					callExiration = System.currentTimeMillis() + (call.getDuracion() * 40 * 100); 
+					status = EstatusPersona.IN_CALL;
+				}
+				
+			}else{
+				
+				if(System.currentTimeMillis() > callExiration){
+					status = EstatusPersona.FREE;
+					logger.info("Supervisor "+id+" Colgando Llamada ... ");
+				}
+			}
+			
+			sleep();
+		}
+		
+	}
+	
+	private void sleep(){
+		try{
+			Thread.sleep(1000);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stop(){
+		running = false;
+	}
+	
+	public void start(){
+		running = true;
+		Thread trThread = new Thread(this);
+		trThread.setPriority(Thread.NORM_PRIORITY);
+		trThread.start();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
